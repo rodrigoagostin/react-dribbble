1d3f00ae7908492ffc6e7a958535f967504f3abb
Rodrigo Agostinho <therodrigoagostin@gmail.com>
2016-07-08 12:34:20 -0300

    prod

---
+++ webpack.prod.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'source-map',
+
+  entry: [
+    './src/index'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+  
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  }
+}
